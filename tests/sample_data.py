ad530ef89d451b634473746c1cc589b0134cb243
sbezrukaviy <sbezrukaviy@zilliant.com>
2024-04-15 16:24:06 +0300

    KAN-6 Add a few basic tests

---
+++ tests/sample_data.py
@@ -0,0 +1,24 @@
+from model.movie import Movie
+from model.quizz import Quizz
+
+
+mock_movies = [
+    Movie(1, 'Harry Potter'),
+    Movie(2, 'Lord of the Rings'),
+    Movie(3, 'Scarface'),
+    Movie(4, 'The Godfather'),
+    Movie(5, 'Shrek'),
+]
+
+mock_quizzes = [
+    Quizz('1', mock_movies[0], mock_movies[1],
+          mock_movies[2], mock_movies[3], mock_movies[4]),
+    Quizz('2', mock_movies[1], mock_movies[0],
+          mock_movies[2], mock_movies[3], mock_movies[4]),
+    Quizz('3', mock_movies[2], mock_movies[0],
+          mock_movies[1], mock_movies[3], mock_movies[4]),
+    Quizz('4', mock_movies[3], mock_movies[0],
+          mock_movies[1], mock_movies[2], mock_movies[4]),
+    Quizz('5', mock_movies[4], mock_movies[0],
+          mock_movies[1], mock_movies[2], mock_movies[3])
+]
