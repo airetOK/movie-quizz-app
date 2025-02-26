cc36838ea854be746d4dae6875f636dd9f370045
sbezrukaviy <sbezrukaviy@zilliant.com>
2025-02-26 19:33:22 +0200

    KAN-15 Missing return to main menu button after completing a quick quizz

--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -24,3 +24,32 @@ def test_quizz(browser: Browser):
     page = context.new_page()
     page.goto(f"{BASE_URL}/quizz")
     expect(page).to_have_url(re.compile(f"{BASE_URL}/quizz"))
+
+
+@open_close_browsers
+def test_return_to_main_menu_from_quick_quizz(browser: Browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(f"{BASE_URL}/quizz")
+    page.locator("#return-to-main-menu").click()
+    expect(page).to_have_url(re.compile(f"{BASE_URL}"))
+
+
+@open_close_browsers
+def test_return_to_main_menu_from_game(browser: Browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(f"{BASE_URL}/start")
+    page.locator("#return-to-main-menu").click()
+    expect(page).to_have_url(re.compile(f"{BASE_URL}"))
+
+
+@open_close_browsers
+def test_return_to_main_menu_from_result_page(browser: Browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(f"{BASE_URL}/quizz")
+    page.locator("div.movies form:first-of-type button").click()
+    expect(page).to_have_url(re.compile(f"{BASE_URL}/quizz/.*/verify"))
+    page.locator("#return-to-main-menu").click()
+    expect(page).to_have_url(re.compile(f"{BASE_URL}"))
